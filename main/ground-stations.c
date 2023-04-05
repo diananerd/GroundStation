01e9c8d6a38cd1db9fa59121162254d8cd29f297
Diana Martinez <diananerdoficial@gmail.com>
2023-04-05 17:27:35 -0600

    Add REPL with nvs commands

--- main/ground-stations.c
+++ main/ground-stations.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "esp_vfs_fat.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "cmd_nvs.h"
 
-void app_main(void)
-{
+static void initialize_nvs(void) {
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK( nvs_flash_erase() );
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+}
 
+void app_main(void) {
+  esp_console_repl_t *repl = NULL;
+  esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+  repl_config.prompt = "[INPUT]:";
+  // repl_config.max_cmdline_length = 1024;
+
+  initialize_nvs();
+
+  /* Register commands */
+  esp_console_register_help_command();
+  register_nvs();
+
+  /* Setup uart repl */
+  esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+
+  /* Run repl */
+  ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
