c5eb4e23ff8466df1d00d177bd4c3f345341cd7b
Diana Martinez <diananerdoficial@gmail.com>
2023-07-04 23:35:53 -0600

    using sudo for esptool

--- merge.sh
+++ merge.sh
@@ -3,4 +3,4 @@ pip install esptool
 cd ./build
 pwd
 ls -a
-python -m esptool --chip esp32 merge_bin -o firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bootloader/bootloader.bin 0x10000 ground-station.bin 0x8000 partition_table/partition-table.bin
+sudo python -m esptool --chip esp32 merge_bin -o firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bootloader/bootloader.bin 0x10000 ground-station.bin 0x8000 partition_table/partition-table.bin
